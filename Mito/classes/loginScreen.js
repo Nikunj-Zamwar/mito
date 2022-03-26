f8fa85a1130576d3c64658bdffb4df220e823e41
Nikunj-Zamwar <89663831+Nikunj-Zamwar@users.noreply.github.com>
2022-03-26 11:18:57 -0500

    Added login screen

---
+++ Mito/classes/loginScreen.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function loginScreen() {
+  return (
+    <View style={styles.container}>
+      <Button style = {styles.Button}>School</Button>
+      <Button>Business</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Button : {
+    position: absolute,
+    left: '0%',
+    right: '0%',
+    top: '0%',
+    bottom: '0%',
+    
+    backgroundColor: '#A0D6BB',
+    borderRadius: '20px',
+  },
+});
