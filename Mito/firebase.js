df14148aed0548664182b60a701fe94e8a1f76d0
theriser101@gmail.com <theriser101@gmail.com>
2022-03-26 11:53:59 -0500

    Cleaned up firebase.js

--- Mito/firebase.js
+++ Mito/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics} from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore/lite'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,4 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
