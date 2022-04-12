0c44d1b8b0830eaf723a71f0b9aeb07bf38ea2ef
theriser101@gmail.com <theriser101@gmail.com>
2022-04-11 19:41:39 -0500

    Added packages we need.

--- Mito/App.js
+++ Mito/App.js
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider, Link, Box } from "native-base"
 import { createStackNavigator, createAppContainer } from 'react-navigation';    
 
-export default function App() {
+export default function App(props) {
   return (
     <NativeBaseProvider>
         <View style={styles.container}>
           <Text>Open up App.js to start working on your app!</Text>
           <StatusBar style="auto" />
-          <Link onPress={() => this.props.navigation.navigate('CheckboxClass')} mt = '8'>
+          <Link onPress={() => props.navigation.navigate('CheckboxClass')} mt = '8'>
             <Box px="3" py="2" bg="primary.400" rounded="sm" _text={{
                 color: "white",
                 fontWeight: "medium"
