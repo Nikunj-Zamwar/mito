76f98bfd9fbe987cccceca2fb69cfeff57e782de
theriser101@gmail.com <theriser101@gmail.com>
2022-04-14 19:26:13 -0500

    Getting there.

--- Mito/classes/homeScreen.js
+++ Mito/classes/homeScreen.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {Box, Link, View} from "native-base"
+import {Container, VStack} from "native-base"
 import {StyleSheet} from "react-native";
-import {StatusBar} from "expo-status-bar";
 
 export default function HomeScreen(props){
     return(
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            <Link onPress={() => props.navigation.navigate('Checkbox Class')} mt = '8'>
-                <Box px="3" py="2" bg="primary.400" rounded="sm" _text={{
-                    color: "white",
-                    fontWeight: "medium"
-                }}>
-                    Press to go to your to do list
-                </Box>
-            </Link>
-        </View>
+        <VStack space={4} alignItems="center">
+            {/*Checklist for Active Meeting*/}
+            <Container>
+
+            </Container>
+            {/*Agenda for Active Meeting*/}
+            <Container>
+
+            </Container>
+        </VStack>
     );
 }
 
