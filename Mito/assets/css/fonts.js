b8b6f519f20d6c8ce9c2aa218ba3e661c020c4bf
theriser101@gmail.com <theriser101@gmail.com>
2022-04-21 18:53:25 -0500

    Frustrating, I'll figure it out later.

---
+++ Mito/assets/css/fonts.js
@@ -0,0 +1,6 @@
+
+const fonts = `<style> 
+    @import url('https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@600&family=Inter:wght@500&display=swap');
+</style>`;
+
+export default fonts;
--- Mito/classes/homeScreen.js
+++ Mito/classes/homeScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {View, Box, VStack, Text} from "native-base"
+import fonts from '../assets/css/fonts.js'
 
 export default function HomeScreen(props){
     return(
         <View>
         <VStack space={4} alignItems="center">
             <Box width="80%" borderRadius="md" borderWidth="2px" borderColor="#A3C1AD" bg="#C4DF9D">
-                <Text pl="15px" pt= "10px" color="#787874" textAlign="left" fontSize="13" fontFamily="Inter:wght@500">work</Text>
-                <Text pl="15px" pt= "5px" color="#656363" textAlign="left" fontSize="20" fontFamily="Baloo+Bhaijaan+2:wght@600">work</Text>
+                <Text pl="15px" pt= "10px" color="#787874" textAlign="left" fontSize="13" fontFamily="Inter">work</Text>
+                <Text pl="15px" pt= "5px" color="#656363" textAlign="left" fontSize="20" fontFamily="Baloo Bhaijaan 2">work</Text>
             </Box>
             {/*Agenda for Active Meeting*/}
 
