0ef7f22c95c6fe7b98b1157b327aa3e2e0c5b87a
theriser101@gmail.com <theriser101@gmail.com>
2022-04-21 19:43:42 -0500

    Fonts should be working now.

--- Mito/App.js
+++ Mito/App.js
@@ -2,15 +2,16 @@ import HomeScreen from './classes/homeScreen.js';
 import { NativeBaseProvider} from "native-base"
 import {createNativeStackNavigator } from '@react-navigation/native-stack';
 import {NavigationContainer} from "@react-navigation/native";
-
 const MainStack = createNativeStackNavigator();
+import {useFonts, BalooBhaijaan2_700Bold} from '@expo-google-fonts/baloo-bhaijaan-2';
 
 export default function App() {
+  useFonts({BalooBhaijaan2_700Bold});
   return (
       <NavigationContainer>
     <NativeBaseProvider>
       <MainStack.Navigator>
-        <MainStack.Screen name = "Home" component = {HomeScreen} />
+        <MainStack.Screen name = "Home" component = {HomeScreen}/>
         {/*<MainStack.Screen name = "Checkbox Class" component = {CheckboxClass} />*/}
       </MainStack.Navigator>
     </NativeBaseProvider>
--- Mito/classes/homeScreen.js
+++ Mito/classes/homeScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {View, Box, VStack, Text} from "native-base"
-import fonts from '../assets/css/fonts.js'
-
+import { useFonts, Inter_400Regular} from '@expo-google-fonts/inter';
+import {BalooBhaijaan2_700Bold} from '@expo-google-fonts/baloo-bhaijaan-2';
 export default function HomeScreen(props){
+    useFonts({Inter_400Regular, BalooBhaijaan2_700Bold});
+
     return(
         <View>
         <VStack space={4} alignItems="center">
             <Box width="80%" borderRadius="md" borderWidth="2px" borderColor="#A3C1AD" bg="#C4DF9D">
-                <Text pl="15px" pt= "10px" color="#787874" textAlign="left" fontSize="13" fontFamily="Inter">work</Text>
-                <Text pl="15px" pt= "5px" color="#656363" textAlign="left" fontSize="20" fontFamily="Baloo Bhaijaan 2">work</Text>
+                <Text pl="15px" pt= "10px" color="#787874" textAlign="left" fontSize="13" fontFamily="Inter_400Regular">work</Text>
+                <Text pl="15px" pt= "5px" color="#656363" textAlign="left" fontSize="20" fontFamily="BalooBhaijaan2_700Bold">work</Text>
             </Box>
             {/*Agenda for Active Meeting*/}
 
