774cea75202644a3a7bca493f56229f894c438be
theriser101@gmail.com <theriser101@gmail.com>
2022-04-24 23:58:43 -0500

    new fonts

--- Mito/App.js
+++ Mito/App.js
@@ -3,15 +3,13 @@ import { NativeBaseProvider} from "native-base"
 import {createNativeStackNavigator } from '@react-navigation/native-stack';
 import {NavigationContainer} from "@react-navigation/native";
 const MainStack = createNativeStackNavigator();
-import {useFonts, BalooBhaijaan2_700Bold} from '@expo-google-fonts/baloo-bhaijaan-2';
 import {CheckBoxTest} from "./classes/testListDisplay";
 
 export default function App() {
-  useFonts({BalooBhaijaan2_700Bold});
   return (
       <NativeBaseProvider>
         <NavigationContainer>
-          <MainStack.Navigator initalRouteName = "HomeScreen">
+          <MainStack.Navigator initalRouteName = "CheckboxClass">
             <MainStack.Screen name = "Home" component = {HomeScreen}/>
             <MainStack.Screen name = "CheckboxClass" component = {CheckBoxTest} />
           </MainStack.Navigator>
