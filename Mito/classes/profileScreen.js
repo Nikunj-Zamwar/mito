caa2d5f6fbee4fcd5ffc91d88277b84836aa48fc
theriser101@gmail.com <theriser101@gmail.com>
2022-05-08 14:13:18 -0500

    A little stuck on how to get this UI positioned properly.

--- Mito/classes/profileScreen.js
+++ Mito/classes/profileScreen.js
@@ -8,7 +8,7 @@ import {doc, onSnapshot} from 'firebase/firestore'
 import {db} from "../firebase.js";
 import {useState, useEffect} from 'react';
 
-const tempProfileImage = require("../assets/tempProfile.png");
+const tempProfileImage = require("../assets/PlantGrowth1.png");
 
 export default function ProfileScreen(){
     let [fontsLoaded] = useFonts({Inter_400Regular, BalooBhaijaan2_600SemiBold, BalooBhaijaan2_500Medium});
@@ -20,19 +20,26 @@ export default function ProfileScreen(){
     const windowHeight = Dimensions.get('window').height;
 
     return(
-        <View alignItems = "center" borderWidth = "1px" bottom = {windowHeight - 450}>
-            <Circle size ={windowWidth * 1.2}  bg="#A0D6BB">
-                <VStack pt = {windowHeight / 3.2} alignItems = "center">
-                    <Text fontSize = "40px" fontFamily = "BalooBhaijaan2_600SemiBold" color = "white">ashley rivera</Text>
-                    <Text fontSize = "32px" fontFamily = "BalooBhaijaan2_500Medium" color = "white">growth level: 2</Text>
+        <ScrollView>
+        <View alignItems = "center">
+            <Flex borderWidth={1} alignItems ="center">
+                <Circle size ={windowWidth * 1.2}  bg="#A0D6BB">
+                    <VStack pt = {windowHeight / 3.2} alignItems = "center">
+                        <Text fontSize = "40px" fontFamily = "BalooBhaijaan2_600SemiBold" color = "white">ashley rivera</Text>
+                        <Text fontSize = "32px" fontFamily = "BalooBhaijaan2_500Medium" color = "white">growth level: 2</Text>
+                        <Flex direction="row">
+                            <Text fontSize = "20px" fontFamily = "Inter_400Regular" color = "white">50 collected</Text>
+                        </Flex>
+                    </VStack>
+                </Circle>
+                <Flex borderWidth = {1} alignContent ="flex-end" height = "50%">
+                    <Image resizeMode = "contain" source={tempProfileImage} alt ="Profile Picture"/>
                     <Flex direction="row">
-                        <Text fontSize = "20px" fontFamily = "Inter_400Regular" color = "white">50 collected</Text>
+                        <Text fontSize = "20px" fontFamily = "Inter_400Regular" color = "#94BDB8">50 needed to grow!</Text>
                     </Flex>
-                </VStack>
-            </Circle>
-            <Flex width = "50%">
-                <Image bottom = "59px" resizeMode = "contain" source={tempProfileImage} alt ="Profile Picture"/>
+                </Flex>
             </Flex>
         </View>
+        </ScrollView>
     );
 }
